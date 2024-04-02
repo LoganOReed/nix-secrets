a7817de52a4bdb99e2842af8c6fcdb4b4e94f952
Logan Reed <me@loganreed.org>
2024-04-02 09:32:14 -0400

    Fixed pub key

--- secrets.nix
+++ secrets.nix
@@ -7,12 +7,12 @@ let
   #     cat ~/.ssh/id_ed25519.pub
   # if you do not have one, you can generate it by command:
   #     ssh-keygen -t ed25519
-  occam = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK6sLwHf6YinWCrt0amlNhrWml3i+Vq7Ju1GcnAWd1zL occam@razor";
+  occam = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK6sLwHf6YinWCrt0amlNhrWml3i+Vq7Ju1GcnAWd1zL";
   users = [ occam ];
 
   # get system's ssh public key by command:
   #    cat /etc/ssh/ssh_host_ed25519_key.pub
-  ai = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPJucYK/TdGJqfqKhubugyx6L6N5RBXvBETJQ/ytLlmt root@razor";
+  ai = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPJucYK/TdGJqfqKhubugyx6L6N5RBXvBETJQ/ytLlmt";
   systems = [ ai ];
 in
 {
